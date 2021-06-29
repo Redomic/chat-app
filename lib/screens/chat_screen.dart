2ddbb7d9701cd25f7f962c0f2805df839dabc0e7
Redomic <redomicmc@gmail.com>
2021-06-29 14:53:29 +0530

    add message functionality

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,26 +4,43 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works'),
-        ),
-        itemCount: 10,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/IezH0IlU7z1aN7Bw2Foa/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = streamSnapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(docs[index]['text']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
+          // FirebaseFirestore.instance
+          //     .collection('chats/IezH0IlU7z1aN7Bw2Foa/messages')
+          //     .snapshots()
+          //     .listen((data) {
+          //       data.docs.forEach((document) {
+          //         print(document['text']);
+          //       });
+          // });
           FirebaseFirestore.instance
               .collection('chats/IezH0IlU7z1aN7Bw2Foa/messages')
-              .snapshots()
-              .listen((data) {
-                data.docs.forEach((document) {
-                  print(document['text']);
-                });
-          });
+              .add({
+                'text': 'Added another entry'
+              });
         },
       ),
     );
