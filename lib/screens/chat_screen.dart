86e1d1ceae4dfa6f7d3d7d913939844d6723ac21
Redomic <redomicmc@gmail.com>
2021-06-29 14:00:06 +0530

    add firebase core to main and firestore snapshot

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: ListView.builder(
         itemBuilder: (ctx, index) => Container(
@@ -11,6 +12,19 @@ class ChatScreen extends StatelessWidget {
           child: Text('This Works'),
         ),
         itemCount: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/IezH0IlU7z1aN7Bw2Foa/messages')
+              .snapshots()
+              .listen((data) {
+                data.docs.forEach((document) {
+                  print(document['text']);
+                });
+          });
+        },
       ),
     );
   }
